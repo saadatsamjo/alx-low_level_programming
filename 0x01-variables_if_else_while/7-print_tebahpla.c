79fbd65cff4a4bb5a161030e74fa485971c790c3
Saadat <samjosaadat@yahoo.com>
2022-06-05 16:24:22 -0700

    ;;

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,19 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+#include <ctype.h>
+
+/**
+ *main - Print the alfabet
+ *
+ *Return: Always (Success)
+ */
+int main(void)
+{int i;
+	for (i = 48; i < 58; i++)
+{
+	putchar(i);
+}
+putchar('\n');
+return (0);
+}
