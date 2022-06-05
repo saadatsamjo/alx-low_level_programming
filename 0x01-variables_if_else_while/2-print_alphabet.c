daac7fb2677225d3492710ee0810fa57764ae3f7
Saadat <samjosaadat@yahoo.com>
2022-06-05 13:41:09 -0700

    ;;

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ *main - Print the alfabet
+ *  
+ *Return: Always (Success)
+ */
+int main(void)
+{char i;
+
+for (i = 'a'; i <= 'z'; i++)
+ {
+	 putchar(i);
+ }
+putchar ('\n');
+return (0)
+}
