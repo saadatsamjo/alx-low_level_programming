06ad135e2cda36b914d0f783bca243b4bb10be98
Saadat <samjosaadat@yahoo.com>
2022-06-05 16:05:15 -0700

    ;;

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,19 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+#include <ctype.h>
+
+/**
+ *main - Print the alfabet
+ *
+ *Return: Always (Success)
+ */
+int main(void)
+{int i;
+	for (i = 0; i < 10; i++)
+{
+	printf("%d", i);
+}
+putchar('\n');
+return (0);
+}
