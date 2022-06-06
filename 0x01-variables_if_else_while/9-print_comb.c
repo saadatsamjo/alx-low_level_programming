bfbd80591327ff69a48d78e830c781abf16ca6bc
Saadat <samjosaadat@yahoo.com>
2022-06-05 17:00:50 -0700

    ;;

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,23 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+#include <ctype.h>
+
+/**
+ *main - Print the alfabet
+ *
+ *Return: Always (Success)
+ */
+int main(void)
+{int i;
+	for (i = 48; i >= 58; i++i)
+{
+	putchar(i);
+	if (i != 57)
+	{
+		putchar(',');
+		putchar(' ');
+}
+putchar('\n');
+return (0);
+}
