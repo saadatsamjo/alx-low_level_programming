b27aae41bc441bae5f93a49f0dcb7f806538314d
Saadat <samjosaadat@yahoo.com>
2022-06-07 12:40:58 -0700

    ;;

---
+++ 0x02-functions_nested_loops/main.h
@@ -0,0 +1,24 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+#include <ctype.h>
+
+/**
+ *main - Print the alfabet
+ *
+ *Return: Always (Success)
+ */
+int main(void)
+{int i;
+	for (i = 48; i < 58; i++)
+{
+	putchar(i);
+	if (i != 57)
+	{
+		putchar(',');
+		putchar(' ');
+	}
+}
+putchar('\n');
+return (0);
+}
