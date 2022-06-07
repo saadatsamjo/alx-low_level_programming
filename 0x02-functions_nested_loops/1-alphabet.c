c4c6ad8e92675b88fee2464cdaeda609f7116fce
Saadat <samjosaadat@yahoo.com>
2022-06-07 13:38:36 -0700

    ;;

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,18 +1,17 @@
 #include "main.h"
-/**
- * main - Prints _putchar as a message.
- * Return: Always 0 (Success)
- */
-int main(void)
-{
-	int str[] = {95, 112, 117, 116, 99, 104, 97, 114};
-	int count, sz;
 
-	sz = sizeof(str) / sizeof(int);
-	for (count = 0; count < sz; count++)
-	{
-		_putchar(str[count]);
-	}
-	_putchar('\n');
-	return (0);
+/**
+ *  * print_alphabet - Prints the alphabet in lowercase.
+ *   *
+ *    * Return: Always 0 (Success)
+ *     */
+void print_alphabet(void)
+{
+		int i;
+
+			for (i = 97; i <= 122; i++)
+					{
+								_putchar(i);
+									}
+				_putchar('\n');
 }
