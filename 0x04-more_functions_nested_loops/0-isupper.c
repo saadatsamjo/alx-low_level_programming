7b539d06b609233eb4e1abbf63945de285f9e040
Saadat <samjosaadat@yahoo.com>
2022-06-10 16:34:02 -0700

    ;;

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - checks if its uppercase
+ * Return: 0
+ */
+
+int main (void)
+{
+	char _isupper(char c)
+	{
+		if ((c >= 65) && (c <= 90 ))
+		{
+			return (1);
+		}
+		else
+		{
+			return(0);
+		}
+	}
+}
