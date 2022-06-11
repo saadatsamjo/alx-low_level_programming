4ab27d01a92e42f9378203ed5757daed17c75e02
Saadat <samjosaadat@yahoo.com>
2022-06-11 07:35:38 -0700

    ;;

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -7,6 +7,7 @@
 void print_most_numbers(void)
 {
 	int i;
+
 	for (i = 48; i < 58; i++)
 	{
 		if (i != 50 && i != 52)
@@ -14,5 +15,5 @@ void print_most_numbers(void)
 			_putchar(i);
 		}
 	}
-	_putchar('\n')
+	_putchar('\n');
 }
