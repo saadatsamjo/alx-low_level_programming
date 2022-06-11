4113ea0103336862df74a5bcbade33b3be8f1cb9
Saadat <samjosaadat@yahoo.com>
2022-06-11 07:14:06 -0700

    ;;

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,18 @@
+int main()
+{
+  int numbers()
+  {
+    for (int N =0; N <= 9; N++)
+      {
+	if (N == 2 || N == 3)
+	  {
+	    continue;
+	  }
+	else
+	  {
+	    _putchar(N);
+	  }
+      }
+    _putchar("\n");
+  }
+}
