60429b67bcf201a2e17f83e933fd1af66179805f
Saadat <samjosaadat@yahoo.com>
2022-06-11 04:10:00 -0700

    ;;

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -6,17 +6,14 @@
 * Return: 0
 */
 
-int main(void)
+char _isupper(char c)
 {
-	char _isupper(char c)
+	if (c >= 65 && c <= 90)
 	{
-		if (c >= 65 && c <= 90)
-		{
-			return (1);
-		}
-		else
-		{
-			return (0);
-		}
+		return (1);
+	}
+	else
+	{
+		return (0);
 	}
 }
