a13217d326b560ed8cd99324caaacb79a13c321d
Saadat <samjosaadat@yahoo.com>
2022-06-13 14:56:34 -0700

    ;;

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _puts - prints the string
+ * @s: parametr
+ * Return: 0
+ *
+ */
+
+int _puts(char *s)
+{
+	int i = 0;
+
+	while(s[i] != '\0')
+		printf("%c",s[i]);
+	i++;
+}
