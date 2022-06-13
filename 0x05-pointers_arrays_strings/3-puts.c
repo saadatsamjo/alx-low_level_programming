89fc2d5cc6c736c06a93855a564ce7590f792801
Saadat <samjosaadat@yahoo.com>
2022-06-13 15:33:38 -0700

    ;;

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -3,7 +3,7 @@
 
 /**
  * _puts - prints the string
- * @s: parametr
+ * @str: parametr
  * Return: 0
  *
  */
@@ -12,7 +12,7 @@ void _puts(char *str)
 {
 	int i = 0;
 
-	while(str[i] != '\0')
+	while (str[i] != '\0')
 	{
 		printf("%c", str[i]);
 		i++;
