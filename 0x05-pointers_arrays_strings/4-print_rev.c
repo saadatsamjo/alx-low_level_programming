536f48db787a7953bfc44e1a6651dd4eab2da58f
Saadat <samjosaadat@yahoo.com>
2022-06-13 16:34:55 -0700

    ;;

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * print_rev - prints string in reverse
+ * @s: prmt
+ * Return: 0
+ *
+ */
+
+char *str;
+str = "thestring";
+void print_rev(char *s)
+{
+	int i;
+
+	for (i=(sizeof(s))/(sizeof(s[0])); i>=0; i--)
+	{
+		printf("%c", s[i]);
+	}
+	printf("\n");
+}
