07578a41c27a30e73171e04331cc3d554e5f0373
Saadat <samjosaadat@yahoo.com>
2022-06-14 11:27:41 -0700

    ;;

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -7,22 +7,22 @@
 
 void rev_string(char *s)
 {
-  int i, size, half;
-  char first, last;
+	int i, size, half;
+	char first, last;
+	i = 0;
 
-  i = 0;
-  while (s[i] != '\0')
-    {
-      i++;
-    }
-  size = i - 1;
-  half = size / 2;
-  while (half >= 0)
-    {
-      first = s[size - half];
-      last = s[half];
-      s[half] = first;
-      s[size - half] = last;
-      half--;
-    }
+       	while (s[i] != '\0')
+	{
+		i++;
+	}
+	size = i - 1;
+	half = size / 2;
+	while (half >= 0)
+	{
+		first = s[size - half];
+		last = s[half];
+		s[half] = first;
+		s[size - half] = last;
+		half--;
+	}
 }
