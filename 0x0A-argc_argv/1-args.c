33bd08c2bdcf695aef19c98e0a3132740fffc86e
Saadat <samjosaadat@yahoo.com>
2022-07-09 13:58:43 -0700

    ;;

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,17 @@
+nclude <stdio.h>
+#include "main.h"
+
+/**
+ * main - Print the number of arguments
+ * @argc: Count arguments
+ * @argv: Arguments
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(int argc, char *argv[])
+{
+	(void) *argv[];
+	printf("%d",argc);
+	return 0;
+}
