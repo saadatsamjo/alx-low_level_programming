22e6b785d6a2915336f0f7ed391f857b4cd145f9
Saadat <samjosaadat@yahoo.com>
2022-07-10 06:11:36 -0700

    ;;

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,13 +1,17 @@
+#ifndef DOG_H
+#define DOG_H
 #include <stdio.h>
+/**
+ * struct dog - new struct
+ * @name:- the name
+ * @age:- the age
+ * @owner:- the owner
+ */
 
-int main()
+struct dog
 {
-	struct dog
-	{
-		char *name;
-		float age;
-		char *owner;
-	} dog;
-	return 0;
-}
-
+	char *name;
+	float age;
+	char *owner;
+} dog;
+#endif
