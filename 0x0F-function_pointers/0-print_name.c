81aa598361648537298ac9672cb3307e07e185f3
Saadat <samjosaadat@yahoo.com>
2022-09-11 07:12:12 -0700

    message

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -3,24 +3,7 @@
 #include <string.h>
 #include "header.h"
 
-void print_name(char *name, void (*f)(char *));
-void print_name_as_is(char *name);
-
-
-int main(void){
-	    void (*f)(char*) = &print_name_as_is;
-	        print_name("leo",f);
-		    return 0;
-}
-
-void print_name_as_is(char *name){
-	    printf("Hello, my name is %s\n", name);
-}
-
-
-
 void print_name(char *name, void (*f)(char *)){
-	    f = &print_name_as_is;
-	        (*f)(name);
+    f = &print_name_as_is;
+    (*f)(name);
 }
-
