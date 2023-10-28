575f00458c440d20b9667c5f0a8e8131033d1758
Saadat <65956764+saadatsamjo@users.noreply.github.com>
2023-10-28 22:53:32 +0000

    Update 4-rev_array.c

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -7,12 +7,16 @@
  * Return: no return.
  */
 
-
-void reverse_array(int *a, int n) {
-    int temp;
-    for (int i = 0; i < n / 2; i++) {
-        temp = a[i];
-        a[i] = a[n - i - 1];
-        a[n - i - 1] = temp;
-    }
+void reverse_array(int *a, int n)
+{
+	int i, j, temp;
+	for (i = 0; i < n - 1; i++)
+	{
+		for (j = i + 1; j > 0; j--)
+		{
+			temp = *(a + j);
+			*(a + j) = *(a + (j - 1));
+			*(a + (j - 1)) = temp;
+		}
+	}
 }
