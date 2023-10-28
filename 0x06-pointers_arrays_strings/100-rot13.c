0483b74b6e93833c599493cd0153bc76e91986d0
Saadat <65956764+saadatsamjo@users.noreply.github.com>
2023-10-28 23:18:52 +0000

    Update 100-rot13.c

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -1,11 +1,12 @@
 #include "main.h"
+#include <stdio.h>
 /**
  * rot13 - encodes a string using rot13
  * @s: input string.
  * Return: the pointer to dest.
  */
 
-char *rot13(char *s)
+/*char *rot13(char *s)
 {
 	int count = 0, i;
 	char alphabet[] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
@@ -22,4 +23,15 @@ char *rot13(char *s)
 	}
 	count++;
 	return (s);
+}*/
+
+
+int main(void) {
+    char text[] = "Hello, World! This is ROT13 encoding.";
+    
+    printf("Original: %s\n", text);
+    rot13(text);
+    printf("ROT13: %s\n", text);
+    
+    return 0;
 }
