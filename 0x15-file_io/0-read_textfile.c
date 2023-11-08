e32241b27633872f47f726f61070d446c0197fba
saadatsamjo <65956764+saadatsamjo@users.noreply.github.com>
2023-11-08 04:50:58 +0000

    added  the main.h

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
+#include "main.h"
 
 ssize_t read_textfile(const char *filename, size_t letters) {
     if (filename == NULL) {
---
+++ 0x15-file_io/main.h
@@ -0,0 +1,6 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+int create_file(const char *filename, char *text_content);
+
+#endif
