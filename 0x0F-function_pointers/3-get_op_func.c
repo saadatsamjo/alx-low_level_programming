23790f73e0c96663d076dec1284cbf8fda219371
Saadat <samjosaadat@eyahoo.com>
2023-11-11 10:56:50 +0000

    the three files

--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -1,73 +1,32 @@
 #include "3-calc.h"
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>  // Include the <string.h> header for strcmp
 
 /**
- * op_add - adds a and b
- * @a: first number
- * @b: second number
+ * get_op_func - selects the correct function to perform the operation
+ * @s: operator passed as argument
  *
- * Return: sum of a and b
+ * Return: pointer to the appropriate function, or NULL if operator not found
  */
-int op_add(int a, int b)
+int (*get_op_func(char *s))(int, int)
 {
-	return (a + b);
-}
+    op_t ops[] = {
+        {"+", op_add},
+        {"-", op_sub},
+        {"*", op_mul},
+        {"/", op_div},
+        {"%", op_mod},
+        {NULL, NULL}
+    };
+    int i = 0;
 
-/**
- * op_sub - subtracts b from a
- * @a: first number
- * @b: second number
- *
- * Return: difference of a and b
- */
-int op_sub(int a, int b)
-{
-	return (a - b);
-}
+    while (ops[i].op != NULL)
+    {
+        if (strcmp(s, ops[i].op) == 0)
+            return (ops[i].f);
+        i++;
+    }
 
-/**
- * op_mul - multiplies a and b
- * @a: first number
- * @b: second number
- *
- * Return: multiplication of a and b
- */
-int op_mul(int a, int b)
-{
-	return (a * b);
-}
-
-/**
- * op_div - divides a by b
- * @a: first number
- * @b: second number
- *
- * Return: integer division of a by b
- */
-int op_div(int a, int b)
-{
-	if (b == 0)
-	{
-		printf("Error\n");
-		exit(100);
-	}
-	return (a / b);
-}
-
-/**
- * op_mod - find the remainder of the division of a by b
- * @a: first number
- * @b: second number
- *
- * Return: remainder of the division of a by b
- */
-int op_mod(int a, int b)
-{
-	if (b == 0)
-	{
-		printf("Error\n");
-		exit(100);
-	}
-	return (a % b);
+    return (NULL);
 }
